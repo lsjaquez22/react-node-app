cea427cb9333d0892307538b295cfb44d9a38d8c
Luis Sanchez <lsjaquez22@gmail.com>
2019-11-28 00:01:01 -0700

    change in index

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get("/api/customers", (req, res) => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
-  app.get("*", (res, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
